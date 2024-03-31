b576d7c96b537f32fd63a9f63bf2f6eed308963c
raiha <68338762+raihalea@users.noreply.github.com>
2024-03-31 18:26:05 +0900

    add CTFd plugins

--- .projenrc.ts
+++ .projenrc.ts
@@ -18,6 +18,32 @@ const cdkProject = new awscdk.AwsCdkTypeScriptApp({
   devDeps: ['@types/aws-lambda'],
 });
 
+cdkProject.addTask('initial-ctfd-repos', {
+  steps: [
+    {
+      name: 'add-ctfd',
+      exec: 'git submodule add https://github.com/CTFd/CTFd.git CTFd',
+      condition: '! git submodule status CTFd >/dev/null',
+    },
+    {
+      name: 'add-ctfd-cloudfornt-plugin',
+      exec: 'git submodule add https://github.com/raihalea/CTFd-CloudFront-signed-url.git CTFd-Plugins/cloudfront',
+      condition: '! git submodule status CTFd-Plugins/cloudfront >/dev/null',
+    },
+    {
+      name: 'copy-plugins',
+      spawn: 'copy-plugins',
+    },
+  ],
+});
+
+cdkProject.addTask('copy-plugins', {
+  exec: 'cp -r ./CTFd-Plugins/* ./CTFd/CTFd/plugins/',
+});
+
+cdkProject.addTask('pull-submodules', {
+  exec: 'git submodule update --init --recursive',
+});
 
 const cloudfrontKeypair = new typescript.TypeScriptProject({
   defaultReleaseBranch: 'main',
